cb9a3a78e3a5eea06cdeaabf68f6c87fc8497eff
FatNepBoi <120290758+FatNepBoi@users.noreply.github.com>
2022-12-13 04:37:52 +0000

    Tests working and updated main
    Two tests with increased discard ratio

--- Trabalho4/Main.hs
+++ Trabalho4/Main.hs
@@ -1,15 +1,23 @@
 -- Autor: Yichen Cao        FC58165
 --        Gonçalo Fernandes FC58194
 
-import Test.QuickCheck
 -- import System.Random ()
-import System.Environment (getArgs)
-import T3 (EstadoJogo(..), posicoesDe, move)
-import Tests (prop_move_door_without_key, prop_move_picks_key, prop_move_exists,
-              prop_move_portals, prop_move_number_of_doors, prop_move_keys,
-              prop_move_limits,prop_move_dimensions)
+
 import Data.List (isPrefixOf)
 import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+import T3 (EstadoJogo (..), move, posicoesDe)
+import Test.QuickCheck
+import Tests
+  ( prop_move_dimensions,
+    prop_move_door_without_key,
+    prop_move_exists,
+    prop_move_keys,
+    prop_move_limits,
+    prop_move_number_of_doors,
+    prop_move_picks_key,
+    prop_move_portals,
+  )
 
 -- import Debug.Trace (trace)
 -- debug :: c -> String -> c
@@ -22,88 +30,94 @@ import System.Directory (doesFileExist)
     - will continue to call parseCmd until termination.
 -}
 main :: IO ()
-main =  do
-        -- regular run: args = 1, fileExists, not -t
-        -- test run: args =1, -t--
-        arguments <- getArgs -- text file name included in args
-        processArgs arguments
-
+main = do
+  -- regular run: args = 1, fileExists, not -t
+  -- test run: args =1, -t--
+  arguments <- getArgs -- text file name included in args
+  processArgs arguments
 
 -- parse arguments, checks their validity and return with appropriate response
 processArgs :: [String] -> IO ()
-processArgs args 
-             | length args > 1 = putStrLn ("Too many args. \n" ++ helpMenu )
-             | file == "-t" = runTest 
-             | file == "help" = putStrLn helpMenu
-             | fileExists <- doesFileExist $ head args = loadFiles file
-             | otherwise = putStrLn ("\""++file ++ "\" does not exist." ++ helpMenu )--loadGame $ head args
-             where file = head args
+processArgs args
+  | length args > 1 = putStrLn ("Too many args. \n" ++ helpMenu)
+  | file == "-t" = runTest
+  | file == "help" = putStrLn helpMenu
+  | fileExists <- doesFileExist $ head args = loadFiles file
+  | otherwise = putStrLn ("\"" ++ file ++ "\" does not exist." ++ helpMenu) --loadGame $ head args
+  where
+    file = head args
 
 -- runs quickcheck tests
 runTest :: IO ()
 runTest = do
-    putStrLn "Checks if the dimensions of the game stay the same after moving."
-    quickCheck prop_move_dimensions
-    putStrLn "Checks if the player stays in bounds after moving."
-    quickCheck prop_move_limits
-    putStrLn "Checks if the number of keys the player doesnt decrease after moving."
-    quickCheck prop_move_keys
-    putStrLn "Checks if the player still exists after moving."
-    quickCheck prop_move_exists
-    putStrLn "Checks if the portals stayed consistent after moving."
-    quickCheck prop_move_portals
-    putStrLn "Checks if the number of doors after a move doesnt increase."
-    quickCheck prop_move_number_of_doors
-    putStrLn "Checks if the player stays in place when moving against a locked door."
-    quickCheck prop_move_door_without_key
-    putStrLn "Checks if the keys array increases when the player picks up a key."
-    quickCheck prop_move_picks_key
+  putStrLn "Checks if the dimensions of the game stay the same after moving."
+  quickCheck prop_move_dimensions
+  putStrLn "Checks if the player stays in bounds after moving."
+  quickCheck prop_move_limits
+  putStrLn "Checks if the number of keys the player doesnt decrease after moving."
+  quickCheck prop_move_keys
+  putStrLn "Checks if the player still exists after moving."
+  quickCheck prop_move_exists
+  putStrLn "Checks if the portals stayed consistent after moving."
+  quickCheck prop_move_portals
+  putStrLn "Checks if the number of doors after a move doesnt increase."
+  quickCheck prop_move_number_of_doors
+  putStrLn "Checks if the player stays in place when moving against a locked door."
+  quickCheckWith stdArgs {maxDiscardRatio = 100} prop_move_door_without_key
+  putStrLn "Checks if the keys array increases when the player picks up a key."
+  quickCheckWith stdArgs {maxDiscardRatio = 100} prop_move_picks_key
 
--- loads a file, assuming it is correctly formatted. 
+-- loads a file, assuming it is correctly formatted.
 -- Then continue to ask for another command
-loadFiles :: String -> IO()
+loadFiles :: String -> IO ()
 loadFiles fileName = do
-                    let gameState = if null fileName -- if null, load default, else load whichever
-                        then loadGame "default.map"
-                        else loadGame fileName
-                    loadedGame <- gameState
-                    print loadedGame
-                    cmd <- getLine
-                    parseCmd cmd loadedGame
-
+  let gameState =
+        if null fileName -- if null, load default, else load whichever
+          then loadGame "default.map"
+          else loadGame fileName
+  loadedGame <- gameState
+  print loadedGame
+  cmd <- getLine
+  parseCmd cmd loadedGame
 
 -- the help menu which is printed when incorrect or when the "help" is called
-helpMenu :: String 
-helpMenu =  unlines ["--------------------[Help Menu]--------------------",
-                     "    ./Main help      -- Shows this command menu.",
-                     "    ./Main [file]    -- Runs game with custom map.",
-                     "    ./Main           -- Runs game with default map.",
-                     "    ./Main -t        -- Runs tests for the game.",
-                     "---------------------------------------------------"]
+helpMenu :: String
+helpMenu =
+  unlines
+    [ "--------------------[Help Menu]--------------------",
+      "    ./Main help      -- Shows this command menu.",
+      "    ./Main [file]    -- Runs game with custom map.",
+      "    ./Main           -- Runs game with default map.",
+      "    ./Main -t        -- Runs tests for the game.",
+      "---------------------------------------------------"
+    ]
 
 {-
 - reads a command and a game state, and then parse command correspondingly.
     - valid commands: move <String>, save <FilePath>, load <FilePath>, exit
 - runs recursively until "exit" command is called.
 -}
-parseCmd :: String -> EstadoJogo -> IO()
+parseCmd :: String -> EstadoJogo -> IO ()
 parseCmd cmd game
-        | cmd == "exit" = return () --putStrLn ("kill!")
-        | "move " `isPrefixOf` cmd = loop $ do return $ move game args --`debug` ("comand:" ++ cmd)
-        | "save " `isPrefixOf` cmd = loop $ do saveGame game args -- save the game
-        | "load " `isPrefixOf` cmd = loop $ do --loadGame args 
-                                            fileExists <- doesFileExist args
-                                            if fileExists 
-                                                then do loadGame args
-                                                else do putStrLn ("\""++ args ++ "\" does not exist. Try again" )
-                                                        return game
-        | otherwise = loop $ do {putStrLn "Invalid command. Please try again: "; return game}
-        where args = drop 5 cmd -- instructions are always 4 letters + " "
-              loop instruction = do newGame <- instruction -- specified based on instruction. instruction is an IO a
-                                    print newGame -- has to update game
-                                    newCmd <- getLine
-                                    parseCmd newCmd newGame
-             
+  | cmd == "exit" = return () --putStrLn ("kill!")
+  | "move " `isPrefixOf` cmd = loop $ do return $ move game args --`debug` ("comand:" ++ cmd)
+  | "save " `isPrefixOf` cmd = loop $ do saveGame game args -- save the game
+  | "load " `isPrefixOf` cmd = loop $ do
+    --loadGame args
+    fileExists <- doesFileExist args
+    if fileExists
+      then do loadGame args
+      else do
+        putStrLn ("\"" ++ args ++ "\" does not exist. Try again")
+        return game
+  | otherwise = loop $ do putStrLn "Invalid command. Please try again: "; return game
+  where
+    args = drop 5 cmd -- instructions are always 4 letters + " "
+    loop instruction = do
+      newGame <- instruction -- specified based on instruction. instruction is an IO a
+      print newGame -- has to update game
+      newCmd <- getLine
+      parseCmd newCmd newGame
 
 {-
 - takes a file name and returns a game state.
@@ -111,19 +125,22 @@ parseCmd cmd game
 -}
 loadGame :: FilePath -> IO EstadoJogo
 loadGame x = do
-        content <- readFile x
-        let fileData = lines content
-        let game = Game {maze = mz,
-                         playerPos = read $ head fileData ::(Int,Int),
-                         keys = fileData !! 1,
-                         portals = posicoesDe mz '@'}
-                        where mz = drop 2 fileData
-        return game --returns new loaded game
-
+  content <- readFile x
+  let fileData = lines content
+  let game =
+        Game
+          { maze = mz,
+            playerPos = read $ head fileData :: (Int, Int),
+            keys = fileData !! 1,
+            portals = posicoesDe mz '@'
+          }
+        where
+          mz = drop 2 fileData
+  return game --returns new loaded game
 
 -- takes the file name and a game state, saves current game state into the file.
 saveGame :: EstadoJogo -> FilePath -> IO EstadoJogo
-saveGame game@(Game maze playerPos keys _) saveTo = 
-        do writeFile saveTo $ unlines [show playerPos,keys] ++ unlines maze -- save to file
-           return game -- returns the same game
-
+saveGame game@(Game maze playerPos keys _) saveTo =
+  do
+    writeFile saveTo $ unlines [show playerPos, keys] ++ unlines maze -- save to file
+    return game -- returns the same game
--- Trabalho4/Tests.hs
+++ Trabalho4/Tests.hs
@@ -2,16 +2,23 @@
 --        Gonçalo Fernandes FC58194
 
 module Tests
-  (prop_move_door_without_key, prop_move_picks_key, prop_move_exists,
-   prop_move_portals, prop_move_number_of_doors, prop_move_keys,
-   prop_move_limits,prop_move_dimensions)
+  ( prop_move_door_without_key,
+    prop_move_picks_key,
+    prop_move_exists,
+    prop_move_portals,
+    prop_move_number_of_doors,
+    prop_move_keys,
+    prop_move_limits,
+    prop_move_dimensions,
+  )
 where
 
 import T3 (EstadoJogo (..), inicializa, move, posicoesDe)
-import Test.QuickCheck (Arbitrary (arbitrary), Property, choose, elements,suchThat)
+import Test.QuickCheck (Arbitrary (arbitrary), Property, choose, elements, suchThat)
 -- import T1 (labirintosGen,labirintos5,contaCaracteres) -- deprecated
-import Test.QuickCheck.Gen (vectorOf,generate)
+import Test.QuickCheck.Gen (generate, vectorOf)
 import Test.QuickCheck.Property ((==>))
+
 -- import Test.QuickCheck.Test (quickCheck)
 
 newtype Movimentos = Movimentos String
@@ -21,30 +28,42 @@ instance Show Movimentos where
 
 instance Arbitrary Movimentos where
   arbitrary = do
-    size <- choose (4, 10)
+    size <- choose (1, 5)
     mov <- vectorOf size $ elements "udlr"
     return $ Movimentos mov
 
-
 instance Arbitrary EstadoJogo where
   arbitrary = do
-    col <- choose (4, 10)
-    row <- choose (4, 10)
+    col <- choose (4, 7)
+    row <- choose (4, 7)
     -- maze <- elements labirintos5
-    let rndGut = suchThat (vectorOf row (vectorOf col items))
-                  (\x -> (countAppearance '@' x == 0 || countAppearance '@' x == 2) &&
-                          countAppearance 'S' x == 1 && countAppearance 'F' x == 1) -- &&
-                          --countAppearance 'a' x == 1 && countAppearance 'b' x == 1 &&
-                          --countAppearance 'c' x == 1) -- it takes too long to tests single-keys. ignored
+    let rndGut =
+          suchThat
+            (vectorOf row (vectorOf col items))
+            ( \x ->
+                (countAppearance '@' x == 0 || countAppearance '@' x == 2)
+                  && countAppearance 'S' x == 1
+                  && countAppearance 'F' x == 1
+                  && countAppearance 'a' x >= 0
+                  && countAppearance 'b' x >= 0
+                  && countAppearance 'c' x >= 0
+                  && countAppearance 'A' x >= 0
+                  && countAppearance 'B' x >= 0
+                  && countAppearance 'C' x >= 0
+            )
     maze <- rndGut
-    let borderify = [replicate (col+2) '*'] ++ [ "*" ++ mazeLine ++ "*" | mazeLine <- maze] ++ [replicate (col+2) '*']
+    let borderify = [replicate (col + 2) '*'] ++ ["*" ++ mazeLine ++ "*" | mazeLine <- maze] ++ [replicate (col + 2) '*']
     return $ inicializa borderify
-    where items = elements "          ****SF@ABCabc"
+    where
+      items = elements "          ****SF@ABCabc"
 
 -- Counts how many times a character appears in a Maze
 countAppearance :: Char -> [String] -> Int
-countAppearance c str = length $ filter (==c) (unlines str)
+countAppearance c str = length $ filter (== c) (unlines str)
 
+-- Counts how many times a character appears in a line
+countAppearanceLine :: Char -> String -> Int
+countAppearanceLine c str = length $ filter (== c) str
 
 -- Checks if the dimensions of the game stay the same after moving
 prop_move_dimensions :: EstadoJogo -> Movimentos -> Bool
@@ -60,7 +79,7 @@ prop_move_limits game@(Game maze playerPos keys portals) (Movimentos dir) = move
     moved_player = T3.playerPos moved
     n = length (T3.maze moved) -1
     m = length (head (T3.maze moved)) -1
-    condition = \(x, y) -> x `elem` [0,n] || y `elem` [0,m]
+    condition = \(x, y) -> x `elem` [0, n] || y `elem` [0, m]
 
 -- Checks if the number of keys the player doesnt decrease after moving
 prop_move_keys :: EstadoJogo -> Movimentos -> Bool
@@ -77,7 +96,10 @@ prop_move_number_of_doors game@(Game maze playerPos keys portals) (Movimentos di
     moved_doors = doorPositions moved_maze
 
 doorPositions :: [String] -> [(Int, Int)]
-doorPositions xs = concat [posicoesDe xs x | x <- ['A', 'B', 'C']]
+doorPositions xs = concatMap (posicoesDe xs) "ABC"
+
+keyPositions :: [String] -> [(Int, Int)]
+keyPositions xs = concatMap (posicoesDe xs) "abc"
 
 -- Checks if the portals stayed consistent after moving
 prop_move_portals :: EstadoJogo -> Movimentos -> Bool
@@ -93,19 +115,18 @@ prop_move_exists game@(Game maze playerPos keys portals) (Movimentos dir) = not 
 
 -- Checks if the keys array increases when the player picks up a key
 prop_move_picks_key :: EstadoJogo -> Movimentos -> Property
-prop_move_picks_key game@(Game maze (x, y) keys portals) (Movimentos dir) = 
-    --  (x+1,y) || (x-1,y) || (x,y+1) || (x,y-1) -- in all four directions, one of them has a key 
-    (fst key_coords == x + 1 && snd key_coords == y && all (=='d') dir) 
-      ==> length keys <= length moved_keys
+prop_move_picks_key game@(Game maze (x, y) keys portals) (Movimentos dir) = (positions && directions) ==> length keys <= length moved_keys
   where
-    key_coords = head $ concatMap (posicoesDe maze) "abca"
+    key_coords = keyPositions maze
     moved_keys = T3.keys (move game dir)
+    positions = (x - 1, y) `elem` key_coords
+    directions = countAppearanceLine 'u' dir >= countAppearanceLine 'd' dir && all (\x -> x == 'u' || x == 'd') dir
 
 -- Checks if the player stays in place when moving against a locked door
 prop_move_door_without_key :: EstadoJogo -> Movimentos -> Property
-prop_move_door_without_key game@(Game maze playerPos keys portals) (Movimentos dir) =
-    (null keys && fst door_coords == fst playerPos + 1 && snd door_coords == snd playerPos && all (=='d') dir) 
-      ==> playerPos == moved_player
+prop_move_door_without_key game@(Game maze playerPos keys portals) (Movimentos dir) = no_keys ==> playerPos `notElem` moved_door_coords
   where
-    door_coords = head $ concatMap (posicoesDe maze) "ABC"
-    moved_player = T3.playerPos (move game dir)
+    moved = move game dir
+    moved_player = T3.playerPos moved
+    moved_door_coords = doorPositions $ T3.maze moved
+    no_keys = countAppearance 'a' maze == 0 && countAppearance 'b' maze == 0 && countAppearance 'c' maze == 0
